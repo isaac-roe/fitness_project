62c38b47c86fee1178669217c7382b4f38115b6f
isaacr34 <isaac.roe@my.simpson.edu>
2022-11-16 10:31:25 -0600

    Updated db connection test

--- dbConnect.php
+++ dbConnect.php
@@ -2,9 +2,10 @@
 
 <html>
 	<?php
-	$servername = "localhost";
-	$username = "username";
-	$password = "password";
+
+	$servername = "fitnessappdb";
+	$username = "admin";
+	$password = "fitnessfreaks";
 
 	$conn = new mysqli($servername, $username, $password);
 
@@ -12,5 +13,16 @@
 		die("Connection failed: " . $conn->connect_error);
 	}
 	echo "Connected successfully";
+
+
+	$sql = "CREATE DATABASE test";
+
+	if ($conn->query($sql) === TRUE) {
+		echo "Database created successfully";
+	} else {
+		echo "Error creating database: " . $conn->error;
+	}
+
+	$conn->close();
 	?>
 </html>
