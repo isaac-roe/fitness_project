eab994089ba3225bb043b03857fdff65a5ab45d9
isaacr34 <isaac.roe@my.simpson.edu>
2022-11-21 11:19:21 -0600

    Updated workout and welcome.php page

--- welcome.php
+++ welcome.php
@@ -1,1 +1,26 @@
-Just a test.
+<!DOCTYPE html>
+<!-- Workout log page -->
+
+<html lang="en">
+<link rel="stylesheet" href="styles_css">
+
+<head>
+	<meta charset="UTF-8">
+
+</head>
+
+<body>
+	<p>
+		Number of sets = <?php echo $_POST["sets"]; ?>
+	</p>
+
+	<p>
+	Bench Press:
+	</p>
+	<form action="welcome.php" method="post">
+		Sets: <input type="text" name="sets"> Reps: <input type="text" name="reps"> Weight: <input type="text" name="weight">
+		<input type="submit">
+	</form>
+
+	Number of sets = <?php echo $_POST["sets"]; ?>
+</body>
